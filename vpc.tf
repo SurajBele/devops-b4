01c2637cbf73eb69bf3a4cb734d518f3941fab10
suraj <surajbele51@gmail.com>
2025-02-05 08:58:50 +0530

    terraform vpc file added

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,17 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+resource "aws_instance" "myec2" {
+  ami = "ami-01816d07b1128cd2d"
+  key_name = "id_rsa"
+  instance_type =  "t2.micro"
+  security_groups = [ "mysg" ]
+  subnet_id = aws_subnet.pub_sub.id
+  tags = {
+    Name = "spiderman instance"
+  }
+}
+
 resource "aws_vpc" "spider_vpc" {
     cidr_block = "10.0.0.0/16"
   tags = {
