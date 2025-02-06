3a6879bdd65d28775afa30ef2ba1256687bd7a79
suraj <surajbele51@gmail.com>
2025-02-06 09:16:08 +0530

    data block added

--- vpc.tf
+++ vpc.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "myec2" {
   ami = "ami-01816d07b1128cd2d"
   key_name = "id_rsa"
   instance_type =  "t2.micro"
-  vpc_security_group_ids = [ "data.aws_security_groups.mysg", ]
+  vpc_security_group_ids = [ "data.aws_security_groups.mysg.id", ]
   subnet_id = aws_subnet.pub_sub.id
   tags = {
     Name = "spiderman instance"
