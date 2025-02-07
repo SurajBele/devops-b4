67def45cbab0a6068b0acaa9ea19546fcaf96ae9
suraj <surajbele51@gmail.com>
2025-02-07 09:45:18 +0530

    module created

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
 output "aws_instance" {
-  value = aws_instance.myec2.public_ip
+  value = module.my_instance.aws_instance.module_instance.public_ip
 }
 
 output "aws_vpc" {
-  value = aws_vpc.spider_vpc.id
+  value = module.my_vpc.aws_vpc.my_vpc.id
 }
-output "mysg_id" {
-  value = data.aws_security_groups.mysg.id
+output "subnet_id" {
+  value = module.my_vpc.aws_subnet.my_subnet.id
 }
