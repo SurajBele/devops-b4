d9fe0f057f6c4c71fc31ba4df9d21b050313ee1c
suraj <surajbele51@gmail.com>
2025-02-08 09:01:21 +0530

    --- Auto Git Commit ---

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,9 +1,9 @@
 output "vpc_id" {
   value = aws_vpc.my_vpc.id
 }
-output "my_subnet" {
-  value = aws_subnet.my_subnet.id
-}
+# output "my_subnet" {
+#   value = aws_subnet.my_subnet.id
+# }
 output "my_subnet" {
 
   value = aws_subnet.my_subnet
