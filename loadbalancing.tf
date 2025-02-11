3310a71537997d7f88853438624b9c1a5d274f2d
Anil Pawar <anilpawar8111997@gmail.com>
2025-02-11 15:18:32 +0530

    create my template

--- loadbalancing.tf
+++ loadbalancing.tf
@@ -91,30 +91,53 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-resource "aws_launch_template" "web_server" {
-  name_prefix   = "web-server-"
-  image_id      = "ami-00bb6a80f01f03502"
+resource "aws_instance" "web_server" {
+  ami = "ami_id"
+  key_name = key
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_sg]
 
   user_data = base64encode(<<-EOF
               #!/bin/bash
               yum update -y
-              yum install -y httpd
+              yum install httpd -y
               systemctl start httpd
               systemctl enable httpd
               echo "<h1>Welcome to My Web Server</h1>" > /var/www/html/index.html
               EOF
   )
 
-  tag_specifications {
-    resource_type = "instance"
 
     tags = {
       Name = "Web Server"
     }
   }
-}
 
+resource "aws_security_group" "my_sg" {
+  name        = "my_sg"
+  description = "Allow HTTP and SSH traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 output "alb_dns_name" {
   value = aws_lb.app_lb.dns_name
 }
